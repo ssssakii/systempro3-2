e0cc207ea3a2c950f145cff690714b4ab7df210d
g15948ts <g15948ts@s30538.tsuda.ac.jp>
2017-05-11 16:51:05 +0900

    testケースをcommit

---
+++ snake-camel/test/junit/tutorial/Test.java
@@ -0,0 +1,12 @@
+package junit.tutorial;
+
+import static org.junit.Assert.*;
+
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		System.out.println("Not  yet implemented");
+	}
+
+}
